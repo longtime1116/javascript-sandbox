a4eebc5042822446596442a3902972d4f4ef577a
longtime1116 <longtime1116@gmail.com>
2018-08-05 14:58:28 +0900

    add import/export examples

---
+++ export.js
@@ -0,0 +1,2 @@
+export default "this is a sample code";
+export const X = "this is X";
---
+++ import.js
@@ -0,0 +1,10 @@
+import A from "./export.js"
+console.log(A) // "this is a sample code"
+
+
+import { X } from "./export.js"
+console.log(X) // "this is X"
+
+
+import * as ALL from './export.js
+console.log(ALL.X) // "this is X"
